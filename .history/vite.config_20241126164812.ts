c66cb1328b846c896847adddb8542d5e7b4fb813
princechaudhary874 <princechaudhary0001001@gmail.com>
2024-11-26 16:53:12 +0545

    final deployment

--- .history/vite.config_20241126164812.ts
+++ .history/vite.config_20241126164812.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  base: '/portfolio/',  // Adjust the base URL for GitHub Pages
+  base: '/portfolio/', // Replace 'portfolio' with your repository name
 });
