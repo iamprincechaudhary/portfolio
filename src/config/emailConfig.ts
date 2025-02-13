ade6463d933a7fd53a251c22917b12d1e531e34f
iamprincechaudhary <princechaudhary0001001@gmail.com>
2025-02-13 10:12:23 +0545

    updating index.html for seo

---
+++ src/config/emailConfig.ts
@@ -0,0 +1,7 @@
+// src/config/emailConfig.ts
+export const EMAILJS_CONFIG = {
+    SERVICE_ID: 'service_yl6ww1a',// Replace with your EmailJS Service ID
+    TEMPLATE_ID: 'template_q4o6ryj', // Replace with your EmailJS Template ID
+    USER_ID: 'lrMDuP6yw8kQ4-pzP' // Replace with your EmailJS User ID
+  };
+  
